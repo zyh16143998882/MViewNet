07c8c03575f88f34117d7c782b8df132a5b44c3c
zyh16143998882 <1614399882@qq.com>
2022-03-22 22:28:10 +0800

    fifth commit

--- script/install.sh
+++ script/install.sh
@@ -1,5 +1,5 @@
 # conda install pytorch torchvision cudatoolkit=11.1 -c 
-pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip install --upgrade KNN_CUDA-0.2-py3-none-any.whl -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip install scikit-image==0.15.0 -U -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip install scipy==1.2.1 -i https://pypi.tuna.tsinghua.edu.cn/simple
+pip install -r requirements.txt
+pip install --upgrade KNN_CUDA-0.2-py3-none-any.whl
+pip install scikit-image==0.15.0 -U
+pip install scipy==1.2.1 -i
